1c6f0feb32da54f8b91c3e79c81faac36b3a6401
CodeYouness <ylijassi@gmail.com>
2024-07-15 23:23:06 +0200

    added header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,50 @@
 <footer>
+    <main>
+        <div class="container-link">
+            <section class="link">
+                <nav>
+                    <h2>dc comics</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Link</a>
+                        </li>
+                    </ul>
+                    <h2>shop</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Link</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <h2>dc</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Link</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav>
+                    <h2>dc</h2>
+                    <ul>
+                        <li>
+                            <a href="#">Link</a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
 
+            <section class="logo">
+                <img src="../assets/img/dc-logo-bg.png" alt="">
+            </section>
+        </div>
+    </main>
+
+    <div>
+        <button>sign-up now!</button>
+        <section>
+            <span>follow us</span>
+            <img v-for="social in socials" :src="social.url" :alt="social.social">
+        </section>
+    </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,23 @@
 <header>
+    <div class="container">
 
+        <section class="image">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </section>
+
+        <section class="list-title">
+            <ul>
+                <li>
+                    <a href="#">Link</a>
+                </li>
+                <li>
+                    <a href="#">Link</a>
+                </li>
+                <li>
+                    <a href="#">Link</a>
+                </li>
+            </ul>
+        </section>
+    </div>
 </header>
+
